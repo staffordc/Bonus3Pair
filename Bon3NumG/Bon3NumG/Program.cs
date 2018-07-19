babe39bf5187ada8bf2b9e504c2c69adb2675818
staffordc <stafford.carh@gmail.com>
2018-07-19 10:19:58 -0400

    class help w methods

--- Bon3NumG/Bon3NumG/Program.cs
+++ Bon3NumG/Bon3NumG/Program.cs
@@ -35,43 +35,15 @@ namespace Bon3NumG
                     GuessCount++;
                     if (Guess != ActNum)
                     {
-
-                        if (Guess - ActNum < 10 && Guess - ActNum > 0)
-                        {
-                            Console.WriteLine("Close, but still high");
-                        }
-                        else if (Guess > ActNum)
-                        {
-                            Console.WriteLine("Too High");
-                        }
-                        else if (ActNum - Guess < 10 && ActNum - Guess > 0)
-                        {
-                            Console.WriteLine("Close, but still low");
-                        }
-                        else if (Guess < ActNum)
-                        {
-                            Console.WriteLine("Too Low");
-                        }
+                        ResultOfGuess(Guess, ActNum);   
                     }
 
 
                     else
                     {
-                        Console.WriteLine("Correct!");
+                        OutputNumberOfGuesses(GuessCount);
                         WrongNum = false;
-                        Console.WriteLine("It took " + GuessCount + " tries");
-                        if (GuessCount > 10)
-                        {
-                            Console.WriteLine("Bad job");
-                        }
-                        else if (GuessCount > 5)
-                        {
-                            Console.WriteLine("ehhh job");
-                        }
-                        else
-                        {
-                            Console.WriteLine("good job.");
-                        }
+                        
                     }
                 }
                 //correct?
@@ -105,6 +77,47 @@ namespace Bon3NumG
                 Console.WriteLine("IDK mang, gimme a y or n plz");
                 return Retry();
 
+            }
+
+        }
+        static void ResultOfGuess(int Guess, int ActNum)
+        {
+
+
+            if (Guess - ActNum < 10 && Guess - ActNum > 0)
+            {
+                Console.WriteLine("Close, but still high");
+            }
+            else if (Guess > ActNum)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (ActNum - Guess < 10 && ActNum - Guess > 0)
+            {
+                Console.WriteLine("Close, but still low");
+            }
+            else if (Guess < ActNum)
+            {
+                Console.WriteLine("Too Low");
+            }
+        }
+        static void OutputNumberOfGuesses(int guessCount)
+        
+        {
+            Console.WriteLine("Correct!");
+            Console.WriteLine("It took " + guessCount + " tries");
+
+            if (guessCount > 10)
+            {
+                Console.WriteLine("Bad job");
+            }
+            else if (guessCount > 5)
+            {
+                Console.WriteLine("ehhh job");
+            }
+            else
+            {
+                Console.WriteLine("good job.");
             }
 
         }
